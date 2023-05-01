413ae742076948d9ce203af3eb480cbb2fe6dde1
luccaugusto <lucca@luccaaugusto.xyz>
2023-05-01 16:35:30 -0300

    clicking images goes to github profile

--- src/features/promptfolio/components/Github.tsx
+++ src/features/promptfolio/components/Github.tsx
@@ -3,8 +3,10 @@ import styles from '../Promptfolio.module.css';
 export function Github() {
 	return (
 		<div>
-			<img className={styles.githubStats} alt="Github Stats for Luccaugusto" height="180em" src="https://github-readme-stats-steel-nine.vercel.app/api?username=luccaugusto&show_icons=true&theme=dracula&include_all_commits=true&count_private=true"/>
-			<img className={styles.githubStats} alt="Github Compact Stats for Luccaugusto"height="180em" src="https://github-readme-stats-steel-nine.vercel.app/api/top-langs/?username=luccaugusto&layout=compact&langs_count=7&theme=dracula"/>
+			<a rel={"noreferrer"} target={"_blank"} href="https://github.com/luccaugusto">
+				<img className={styles.githubStats} alt="Github Stats for Luccaugusto" height="180em" src="https://github-readme-stats-steel-nine.vercel.app/api?username=luccaugusto&show_icons=true&theme=dracula&include_all_commits=true&count_private=true"/>
+				<img className={styles.githubStats} alt="Github Compact Stats for Luccaugusto"height="180em" src="https://github-readme-stats-steel-nine.vercel.app/api/top-langs/?username=luccaugusto&layout=compact&langs_count=7&theme=dracula"/>
+			</a>
 		</div>
 	);
 }
