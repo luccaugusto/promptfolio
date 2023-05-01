1263bb984bc0affe1406eda6bc8e475643dd8816
luccaugusto <lucca@luccaaugusto.xyz>
2023-05-01 16:47:36 -0300

    begin development of skillset command

---
+++ src/features/promptfolio/components/Skillset.tsx
@@ -0,0 +1,36 @@
+
+const skillTable = {
+	javascript: {
+		experience: "Work Experience",
+		level: "Advanced",
+		comment: "",
+		interested: true,
+	},
+	typescript: {
+		experience: "Work Experience",
+		level: "Advanced",
+		comment: "",
+		interested: true,
+	},
+	python: {
+		experience: "Work Experience",
+		level: "Advanced",
+		comment: "Experience with Django framework",
+		interested: true,
+	},
+	php: {
+		experience: "Work Experience",
+		level: "Advanced",
+		comment: "Worked a lot with wordpress websites",
+		interested: false,
+	}
+}
+
+
+export function Skillset() {
+
+	return (
+		<div>
+		</div>
+	);
+}
