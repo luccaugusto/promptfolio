e1d48f3bd5d144a31655ecfa41c0daad9300462e
luccaugusto <lucca@luccaaugusto.xyz>
2026-06-22 14:16:41 -0300

    add more videos to youpoop component

--- src/features/youpoop/YouPoop.tsx
+++ src/features/youpoop/YouPoop.tsx
@@ -1,16 +1,28 @@
-import styles from './YouPoop.module.css';
+import { useMemo } from "react";
+import styles from "./YouPoop.module.css";
 
-const VIDEO_ID = 'xfuIlmywvXI';
+const VIDEO_IDS = [
+  "xfuIlmywvXI", // 10 hours OIIAIAOIIIAI
+  "f97W-nveCOM", // 24 hours of Shreksophone
+  "3MU_6BPKmBg", // Mozart 2x speed left ear, the bible in chinese right ear
+  "5mGuCdlCcNM", // 10 hours of bouncing dvd logo
+  "tCHYrpiqDxI", // 10 hours of cockroach dancing to la cucaracha
+];
 
 export function YouPoop() {
-	return (
-		<div className={styles.wrapper}>
-			<iframe
-				title="YouPoop"
-				src={`https://www.youtube.com/embed/${VIDEO_ID}?autoplay=1&loop=1&playlist=${VIDEO_ID}&mute=1`}
-				allow="autoplay; encrypted-media; fullscreen"
-				allowFullScreen
-			/>
-		</div>
-	);
+  const videoId = useMemo(
+    () => VIDEO_IDS[Math.floor(Math.random() * VIDEO_IDS.length)],
+    [],
+  );
+
+  return (
+    <div className={styles.wrapper}>
+      <iframe
+        title="YouPoop"
+        src={`https://www.youtube.com/embed/${videoId}?autoplay=1&loop=1&playlist=${videoId}&mute=1`}
+        allow="autoplay; encrypted-media; fullscreen"
+        allowFullScreen
+      />
+    </div>
+  );
 }
